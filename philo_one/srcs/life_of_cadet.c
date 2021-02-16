35242ed4a680785658cc50a4df07d59f547a3540
Soohyun Choi <soohchoi@c2r7s1.42seoul.kr>
2021-02-17 02:31:07 +0900

    holy

--- philo_one/srcs/life_of_cadet.c
+++ philo_one/srcs/life_of_cadet.c
@@ -6,7 +6,7 @@
 /*   By: soohchoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 23:00:58 by soohchoi          #+#    #+#             */
-/*   Updated: 2021/02/12 23:01:00 by soohchoi         ###   ########.fr       */
+/*   Updated: 2021/02/17 02:30:39 by soohchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,24 @@ int				weekly_report(t_cadet *cadet, int content)
 	return (0);
 }
 
+void			cadet_code_n_sleep_last(t_cadet *cadet)
+{
+	pthread_mutex_lock(cadet->hand_right);
+	weekly_report(cadet, CONTENT_KEYBOARD);
+	pthread_mutex_lock(cadet->hand_left);
+	weekly_report(cadet, CONTENT_KEYBOARD);
+	weekly_report(cadet, CONTENT_CODING);
+	cadet->time_last_code = relative_time(0);
+	spend_time(cadet->time_to_code);
+	if (cadet->num_of_subject >= 0)
+		cadet->num_of_subject++;
+	pthread_mutex_unlock(cadet->hand_right);
+	pthread_mutex_unlock(cadet->hand_left);
+	weekly_report(cadet, CONTENT_SLEEP);
+	spend_time(cadet->time_to_sleep);
+	weekly_report(cadet, CONTENT_OASIS);
+}
+
 void			cadet_code_n_sleep(t_cadet *cadet)
 {
 	pthread_mutex_lock(cadet->hand_left);
@@ -67,25 +85,28 @@ void			*life_of_cadet(void *data)
 		cadet_code_n_sleep(cadet);
 }
 
-int				move_it_cadet(t_bocal *polabear)
+int				move_it_cadet(t_bocal *polarbear)
 {
 	int			i;
 	pthread_t	th_cadet[THREAD_MAX];
 
 	i = 0;
-	while (i < polabear->num_of_cadet)
+	while (i < polarbear->num_of_cadet - 1)
 	{
-		pthread_create(&th_cadet[i], 0, life_of_cadet, &polabear->cadet[i]);
+		pthread_create(&th_cadet[i], 0, life_of_cadet, &polarbear->cadet[i]);
 		pthread_detach(th_cadet[i]);
 		i += 2;
 	}
 	usleep(1000);
 	i = 1;
-	while (i < polabear->num_of_cadet)
+	while (i < polarbear->num_of_cadet - 1)
 	{
-		pthread_create(&th_cadet[i], 0, life_of_cadet, &polabear->cadet[i]);
+		pthread_create(&th_cadet[i], 0, life_of_cadet, &polarbear->cadet[i]);
 		pthread_detach(th_cadet[i]);
 		i += 2;
 	}
+	i = polarbear->num_of_cadet - 1;
+	pthread_create(&th_cadet[i], 0, life_of_cadet, &polarbear->cadet[i]);
+	pthread_detach(th_cadet[i]);
 	return (0);
 }
